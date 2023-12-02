708b115da491bb8f3002861ca49618da4d7d8238
mhmmdnasruloh <mn174539@gmail.com>
2023-12-02 12:37:23 +0700

    first commit

---
+++ application/views/welcome.php
@@ -0,0 +1,6 @@
+<div class="content-wrapper">
+    <section class="content">
+       <div class="btn btn-lg btn-warning"><span class="glyphicon glyphicon-check"></span> XXX Puskesmas BSI</div>
+        
+    </section>
+</div>
